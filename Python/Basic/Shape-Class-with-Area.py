2670844cad496c00a8c4bfdbc7eb293ed7aa5b54
km12 <125912478+km-devlab@users.noreply.github.com>
2026-03-22 22:29:39 +0530

    Update Shape-Class-with-Area.py

--- Python/Basic/Shape-Class-with-Area.py
+++ Python/Basic/Shape-Class-with-Area.py
@@ -1,1 +1,15 @@
+    
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
 
+    def area(self):
+        return math.pi * self.radius ** 2
+
+class Rectangle(Shape):
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
+
+    def area(self):
+        return self.length * self.width
